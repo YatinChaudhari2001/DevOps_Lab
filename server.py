26731e8cc2682f6a5e4e6799f5d38678adf78ec3
yatin <yatinchaudhari13@gmail.com>
2023-07-29 10:24:50 +0530

    just the color has been changed...!

--- server.py
+++ server.py
@@ -8,11 +8,11 @@ def root():
 
 @app.route("/modules", methods=["GET"])
 def module():
-	return '<h3 style="color:blue">DITISS module names :- 1.FCN 2.COSA 3.Security Concepts 4.DevOps 5.NDC 6.PKI 7.Cyber Forensics 8.Compliance Audit</h1>'
+	return '<h3 style="color:orange">DITISS module names :- 1.FCN 2.COSA 3.Security Concepts 4.DevOps 5.NDC 6.PKI 7.Cyber Forensics 8.Compliance Audit</h1>'
 
 @app.route("/me", methods=["GET"]) 
 def me():
-  return '<h1 style="color:blue">230344223055, Yatin Suresh Chaudhari, 8104694273</h1>'
+  return '<h1 style="color:purple">230344223055, Yatin Suresh Chaudhari, 8104694273</h1>'
 
 app.run(port=4000, host="0.0.0.0", debug=True)  
 
